c664ab37255a427abbe0a385c62bc0bcdab09e6e
Louis Roehrs <lroehrs@gmail.com>
2026-04-25 18:53:53 -0700

    Arcade on any screen.

---
+++ wtv/scaler.js
@@ -0,0 +1,16 @@
+/* Now we can play the games on any sized device and full screen on a tv. */
+
+function setZoom() {
+  var scale = window.innerWidth / 560;
+
+  const vscale = window.innerHeight/ 420;
+  if (vscale < scale) {
+    scale = vscale;
+  }
+
+  document.body.style.zoom = scale;
+  document.body.style.display = "flex";
+}
+
+window.addEventListener('load',setZoom);
+window.addEventListener('resize', setZoom);
